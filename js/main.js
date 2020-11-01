2688db7c0eccae07a97eb828fbfba501c011f185
Sadman_Sakib <61703195+Sadman-Sakib2234@users.noreply.github.com>
2020-11-01 13:56:44 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// AlertBox Vari
+
+var alertBox = document.querySelector(".alertbox");
+
+// Getting Password
+
+function getPassword() {
+  var chars =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[]";
+
+  var passwordLength = 16;
+  var password = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+
+    password += chars.substring(randomNumber, randomNumber + 1);
+
+    alertBox.innerHTML = "New PassWord Copied: <br> " + password;
+  }
+
+  document.getElementById("password").value = password;
+}
+
+// Copying PassWord
+
+function copyPassword() {
+  var copyPassText = document.getElementById("password");
+  copyPassText.select();
+  copyPassText.setSelectionRange(0, 9999);
+  document.execCommand("copy");
+  alertBox.classList.toggle("active");
+
+  setTimeout(function () {
+    alertBox.classList.toggle("active");
+  }, 2000);
+}
